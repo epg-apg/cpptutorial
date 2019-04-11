dfb08741cdb6d2b99451a02dcc99f51210590d3a
Pieger <Andreas.Pieger@epg.com>
2019-04-11 07:20:42 +0200

    Output text exercise

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 
 int main()
 {
+    std::cout << "Starting..." << std::flush;
     std::cout << "Hallo !!! Wow !" << std::endl;
+    std::cout << "123 " << "456 " << "789" << std::endl;
+    std::cout << "Another line!" << std::endl;
     return 0;
 }
