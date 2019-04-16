fc77c2a040e58390ab72cbf46df669d54caab102
Pieger <Andreas.Pieger@epg.com>
2019-04-16 16:29:40 +0200

    characters and bool

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <climits>
 
@@ -31,16 +32,16 @@ int main()
     // string input
     std::cout << "Enter your name: " << std::flush;
     
-    std::string input;
-    std::cin >> input;
-    std::cout << "You entered: " << input << std::endl;
+    // std::string input;
+    // std::cin >> input;
+    // std::cout << "You entered: " << input << std::endl;
 
     // numeric input
     std::cout << "Enter a number: " << std::flush;
     
-    int inputNo;
-    std::cin >> inputNo;
-    std::cout << "You entered: " << inputNo << std::endl;
+    // int inputNo;
+    // std::cin >> inputNo;
+    // std::cout << "You entered: " << inputNo << std::endl;
 
     // integer variables
     int willnotwork = 12147483647;
@@ -57,6 +58,36 @@ int main()
 
     unsigned int myuint = 123;
     int myint = 123;
+
+    // floating point
+    float myfloat = 76.4;
+    std::cout << std::scientific << "Float: " << myfloat << std::endl;
+    std::cout << std::setprecision(20) << std::fixed << "Float: " << myfloat << std::endl;
+
+    double mydouble = 76.4;
+    std::cout << std::scientific << "Double: " << mydouble << std::endl;
+    std::cout << std::setprecision(20) << std::fixed << "Double: " << mydouble << std::endl;
+
+    long double myldouble = 76.4;
+    std::cout << std::scientific << "Long Double: " << myldouble << std::endl;
+    std::cout << std::setprecision(20) << std::fixed << "Long Double: " << myldouble << std::endl;
+
+    std::cout << "Sizeof(double): " << sizeof(double) << std::endl;
+    std::cout << "Sizeof(long double): " << sizeof(long double) << std::endl;
+
+    // primitive types
+    bool mybool = true;
+    std::cout << "Bool: " << mybool << std::endl;
+
+    char mychar = 55;
+    std::cout << "Char number: " << mychar << std::endl;
+
+    char mychar2 = '7';
+    std::cout << "Char: " << mychar2 << std::endl;
+    std::cout << "Char number cast: " << (int)mychar2 << std::endl;
+
+    wchar_t wValue = 'i';
+    std::cout << "WChar: " << wValue << std::endl;
 
     return 0;
 }
