ffd1fdf367e7002652fb6934bca61c9a267f3fd6
Pieger <Andreas.Pieger@epg.com>
2019-04-16 06:57:57 +0200

    console input

--- main.cpp
+++ main.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Starting..." << std::flush;
-    std::cout << "Hallo !!! Wow !" << std::endl;
-    std::cout << "123 " << "456 " << "789" << std::endl;
-    std::cout << "Another line!" << std::endl;
+    // integer
+    int numberVar = 123;
+    int numberNew = 456;
+    int both = numberVar + numberNew;
+
+    std::cout << "Hello " << numberVar << std::endl;
+    std::cout << "New " << numberNew << std::endl;
+
+    std::cout << "Both " << numberVar + numberNew << std::endl;
+    std::cout << "Both " << both << std::endl;
+
+    numberNew = numberNew + 1;
+    std::cout << "New " << numberNew << std::endl;
+
+    // string
+    std::string mytext1 = "Hello ";
+    std::string mytext2 = "string. ";
+    std::cout << mytext1 << std::endl;
+    std::cout << mytext2 << std::endl;
+    std::cout << mytext1 << mytext2 << std::endl;
+
+    std::string bothstr = mytext1 + mytext2;
+    std::cout << bothstr << std::endl;
+
+    // string input
+    std::cout << "Enter your name: " << std::flush;
+    
+    std::string input;
+    std::cin >> input;
+    std::cout << "You entered: " << input << std::endl;
+
+    // numeric input
+    std::cout << "Enter a number: " << std::flush;
+    
+    int inputNo;
+    std::cin >> inputNo;
+    std::cout << "You entered: " << inputNo << std::endl;
+
     return 0;
 }
