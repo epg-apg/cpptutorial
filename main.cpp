f4803e0fe716008ddaffac2e15fc301374bb26f5
Pieger <Andreas.Pieger@epg.com>
2019-05-21 10:54:46 +0200

    two dimensional array

--- main.cpp
+++ main.cpp
@@ -271,5 +271,19 @@ int main()
         std::cout << textsArray[i] << std::endl;
     }
 
+    std::string animals[2][3] = {
+        {"fox", "dog", "cat"}, 
+        {"mouse", "squirrel", "parrot"}
+    };
+
+    for(int i=0;i<2;++i)
+    {
+        for(int j=0;j<3;++j)
+        {
+            std::cout << animals[i][j] << " " << std::flush;
+        }
+        std::cout << std::endl;
+    }
+
     return 0;
 }
