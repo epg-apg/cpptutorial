3bb75d60ce749e404776cc24bfb0ab6f596af549
Andreas Pieger <andreas.pieger@epg.com>
2020-04-14 16:26:24 +0200

    Added functions from step 1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
 
+void showMenu()
+{
+    std::cout << "1. Search" << std::endl;
+    std::cout << "2. View" << std::endl;
+    std::cout << "3. Quit" << std::endl;
+}
+
+void processSelection()
+{
+    std::cout << "Enter selection: " << std::endl;
+
+    int input;
+    std::cin >> input;
+
+    switch (input)
+    {
+    case 1:
+        std::cout << "Searching ..." << std::endl;
+        break;
+    case 2:
+        std::cout << "Viewing ..." << std::endl;
+        break;
+    case 3:
+        std::cout << "Quitting ..." << std::endl;
+        break;
+    default:
+        std::cout << "Please select an item from the menu" << std::endl;
+    }
+}
+
 int main()
 {
-    std::cout << "Starting..." << std::flush;
-    std::cout << "Hallo !!! Wow !" << std::endl;
-    std::cout << "123 " << "456 " << "789" << std::endl;
-    std::cout << "Another line!" << std::endl;
+    showMenu();
+    processSelection();
+
     return 0;
 }
