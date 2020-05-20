ec41f250f0b9d3950bb7662442466f7c67626404
Andreas Pieger <andreas.pieger@epg.com>
2020-05-20 14:50:45 +0200

    char array, step 5

--- main.cpp
+++ main.cpp
@@ -2,40 +2,32 @@
 
 int main()
 {
-    const int ELEMENTS          = 5;
-    std::string texts[ELEMENTS] = { "one", "two", "three", "four", "five" };
+    std::string text = "hello";
+    std::cout << text << std::endl;
 
-    std::string *pText = texts;
+    char chars1[] = { 'a', 'b', 'c', 'd', 'e', 0 }; // output is abcde³■a without the 0
+    std::cout << chars1 << std::endl;
 
-    std::cout << "1: " << *pText << std::endl;
-    pText += 3;
-    std::cout << "2: " << *pText << std::endl;
-    pText -= 1;
-    std::cout << "3: " << *pText << std::endl;
+    char chars2[] = "qwertz"; // append 0 to string for null terminator, always when using "
+    std::cout << chars2 << std::endl;
 
-    std::string *pEnd = &texts[ELEMENTS - 1];
-    std::cout << "4: " << *pEnd << std::endl;
-
-    pText = &texts[0];
-    // used for "end of array", but should not be used for anything else, invalid memory area!!!
-    std::string *pInvalidEnd = &texts[ELEMENTS];
-
-    while (pText != pInvalidEnd)
+    for (int i = 0; i < sizeof(chars2); ++i) // last entry is null terminator 0
     {
-        std::cout << "Loop: " << *pText << std::endl;
-        pText++;
+        std::cout << i << ": " << chars2[i] << " - " << (int)chars2[i] << std::endl;
     }
 
-    // number of array items, pointer arithmetic. take last pointer and first pointer and you know
-    // how many are there
-    pText        = &texts[0];
-    long entries = (long)(pInvalidEnd - pText);
-    std::cout << "Entries: " << entries << std::endl;
+    std::cout << "Loop: " << std::flush;
 
-    // pointer arithmetic for middle of array
-    pText = &texts[0];
-    pText += ELEMENTS / 2;
-    std::cout << "Middle: " << *pText << std::endl;
+    int j = 0;
+    while (true)
+    {
+        if (chars2[j] == 0) // end if null terminator is found
+            break;
+
+        std::cout << chars2[j] << std::flush;
+        j++;
+    }
+    std::cout << std::endl;
 
     return 0;
 }
