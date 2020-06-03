3f674ba6b607f2ca425fa396061efec302ef5f1c
Andreas Pieger <andreas.pieger@epg.com>
2020-06-03 16:49:58 +0200

    reverse with pointer, step 6

--- main.cpp
+++ main.cpp
@@ -2,32 +2,40 @@
 
 int main()
 {
-    std::string text = "hello";
-    std::cout << text << std::endl;
+    char text[] = "hello";
+    char reverse[sizeof(text)];
 
-    char chars1[] = { 'a', 'b', 'c', 'd', 'e', 0 }; // output is abcde³■a without the 0
-    std::cout << chars1 << std::endl;
+    // reverse[0]=text[4];
+    // reverse[1]=text[3];
+    // reverse[2]=text[2];
+    // reverse[3]=text[1];
+    // reverse[4]=text[0];
 
-    char chars2[] = "qwertz"; // append 0 to string for null terminator, always when using "
-    std::cout << chars2 << std::endl;
-
-    for (int i = 0; i < sizeof(chars2); ++i) // last entry is null terminator 0
+    for (unsigned int i = 0; i < (sizeof(text) - 1); ++i)
     {
-        std::cout << i << ": " << chars2[i] << " - " << (int)chars2[i] << std::endl;
+        reverse[i] = text[sizeof(text) - 2 - i];
+    }
+    reverse[sizeof(reverse) - 1] = 0;
+
+    std::cout << "Reverse: '" << reverse << "'; '" << text << "'" << std::endl;
+
+    // solution from video with pointer
+    int nChars   = sizeof(text) - 1;
+    char *pStart = text;
+    char *pEnd   = text + nChars - 1;
+
+    std::cout << "Last letter: " << *pEnd << std::endl;
+
+    while(pStart < pEnd) {
+        char save = *pStart;
+        *pStart = *pEnd;
+        *pEnd = save;
+
+        pStart++;
+        pEnd--;
     }
 
-    std::cout << "Loop: " << std::flush;
-
-    int j = 0;
-    while (true)
-    {
-        if (chars2[j] == 0) // end if null terminator is found
-            break;
-
-        std::cout << chars2[j] << std::flush;
-        j++;
-    }
-    std::cout << std::endl;
+    std::cout << "Swapped: " << text << std::endl;
 
     return 0;
 }
