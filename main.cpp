e0568cbf1d1157861c49201eb56c3e4c79cf1533
Andreas Pieger <andreas.pieger@epg.com>
2020-07-23 09:36:03 +0200

    Merge master to functions

--- main.cpp
+++ main.cpp
@@ -1,46 +1,10 @@
 #include <iostream>
-#include "main.h"
 
 int main()
 {
-    showMenu();
-    int selection = getSelection();
-    processSelection(selection);
-
+    std::cout << "Starting..." << std::flush;
+    std::cout << "Hallo !!! Wow !" << std::endl;
+    std::cout << "123 " << "456 " << "789" << std::endl;
+    std::cout << "Another line!" << std::endl;
     return 0;
-}
-
-void showMenu()
-{
-    std::cout << "1. Search" << std::endl;
-    std::cout << "2. View" << std::endl;
-    std::cout << "3. Quit" << std::endl;
-}
-
-int getSelection()
-{
-    std::cout << "Enter selection: " << std::endl;
-
-    int input;
-    std::cin >> input;
-
-    return input;
-}
-
-void processSelection(int selection)
-{
-    switch (selection)
-    {
-    case 1:
-        std::cout << "Searching ..." << std::endl;
-        break;
-    case 2:
-        std::cout << "Viewing ..." << std::endl;
-        break;
-    case 3:
-        std::cout << "Quitting ..." << std::endl;
-        break;
-    default:
-        std::cout << "Please select an item from the menu" << std::endl;
-    }
 }
