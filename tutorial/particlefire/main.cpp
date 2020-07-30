3555610f471f327a488f97514da88714f85c4bee
Andreas Pieger <andreas.pieger@epg.com>
2020-07-30 06:58:37 +0200

    Add SDL example

--- tutorial/particlefire/main.cpp
+++ tutorial/particlefire/main.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 
+#define SDL_MAIN_HANDLED
 #include "SDL2/SDL.h"
 
-int main()
+int main(int argc, char *argv[]) 
 {
-    
+    // Start SDL2
+    SDL_Init(SDL_INIT_EVERYTHING);
+ 
+    // Create a Window in the middle of the screen
+    SDL_Window *window = 0;
+ 
+    window = SDL_CreateWindow("Hello World!",
+                              SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              640, 480,
+                              SDL_WINDOW_SHOWN);
+ 
+    // Delay so that we can see the window appear
+    SDL_Delay(2000);
+ 
+    // Cleanup and Quit
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
